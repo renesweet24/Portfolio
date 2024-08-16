d4d0231373d8e0a28d54b1a1afd9ba2076c595d2
skydev111 <webwizard618@gmail.com>
2024-08-16 08:53:36 -0700

    fix: linkedin address

--- components/intro.tsx
+++ components/intro.tsx
@@ -86,7 +86,7 @@ export default function Intro() {
 
         <a
           className="group bg-gray-950 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-105 hover:scale-105 active:scale-105 transition border-2 border-white border-opacity-40"
-          href="https://www.linkedin.com/ln/connectrenesweet/"
+          href="https://www.linkedin.com/in/connectrenesweet/"
           target="_blank"
         >
           <span className="opacity-70">LinkedIn</span>
